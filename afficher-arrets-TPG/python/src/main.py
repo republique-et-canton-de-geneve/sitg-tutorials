c87a65f81c795b56823a218ed1d5db35462bc2ac
Alessandro Cerioni <alessandro.cerioni@etat.ge.ch>
2025-05-26 10:22:13 +0200

    Remove unused imports

--- afficher-arrets-TPG/python/src/main.py
+++ afficher-arrets-TPG/python/src/main.py
@@ -1,13 +1,14 @@
-import geopandas as gpd
-import pandas as pd
 import folium
-import requests
-import zipfile
+import geopandas as gpd
 import os
 
 ## config
-STOPS_URL = "zip+https://ge.ch/sitg/geodata/SITG/OPENDATA/TPG_ARRETS-GDB.zip!TPG_ARRETS.gdb"
-MUNICIPALITIES_URL = "zip+https://ge.ch/sitg/geodata/SITG/OPENDATA/CAD_COMMUNE-GDB.zip!CAD_COMMUNE.gdb"
+STOPS_URL = (
+    "zip+https://ge.ch/sitg/geodata/SITG/OPENDATA/TPG_ARRETS-GDB.zip!TPG_ARRETS.gdb"
+)
+MUNICIPALITIES_URL = (
+    "zip+https://ge.ch/sitg/geodata/SITG/OPENDATA/CAD_COMMUNE-GDB.zip!CAD_COMMUNE.gdb"
+)
 
 # Step 1: load public transport stops dataset
 print("Loading public transport stops...")
@@ -20,11 +21,15 @@ municipalities_gdf = gpd.read_file(MUNICIPALITIES_URL)
 
 # Step 3: select municipalities
 print("Selecting municipalities...")
-selected_municipalities_gdf = municipalities_gdf[municipalities_gdf['COMMUNE'].isin(['Bernex', 'Meyrin'])].copy()
+selected_municipalities_gdf = municipalities_gdf[
+    municipalities_gdf["COMMUNE"].isin(["Bernex", "Meyrin"])
+].copy()
 
 # Step 4: perform spatial join in order to find stops within the selected municipalities (Bernex and Meyrin)
 print("Filtering stops within selected municipalities...")
-filtered_stops_gdf = stops_gdf.sjoin(selected_municipalities_gdf, how='inner', predicate='intersects')[stops_gdf.columns]
+filtered_stops_gdf = stops_gdf.sjoin(
+    selected_municipalities_gdf, how="inner", predicate="intersects"
+)[stops_gdf.columns]
 
 # Step 5: Create a map of Geneva and add municipal boundaries
 print("Generating map...")
@@ -32,19 +37,20 @@ geneva_map = folium.Map(location=[46.2044, 6.1432], zoom_start=12)
 
 ## Step 5.1: Add municipalities borders in red
 folium.GeoJson(
-    municipalities_gdf.geometry,
-    style_function=lambda x: {'color': 'red', 'weight': 2}
+    municipalities_gdf.geometry, style_function=lambda x: {"color": "red", "weight": 2}
 ).add_to(geneva_map)
 
 ## Step 5.2: Add stop names to the map
 for _, row in filtered_stops_gdf.to_crs(epsg=4326).iterrows():
     folium.Marker(
         location=[row.geometry.y, row.geometry.x],
-        popup=row['NOM_ARRET']  # Stop name
+        popup=row["NOM_ARRET"],  # Stop name
     ).add_to(geneva_map)
 
 # Step 6: Save the map to an HTML file
 print("Saving map to file...")
-os.makedirs('output', exist_ok=True)
-geneva_map.save('output/index.html')
-print("\nDone! A web map has been saved to `output/index.html`. Please check it out ;-)")
+os.makedirs("output", exist_ok=True)
+geneva_map.save("output/index.html")
+print(
+    "\nDone! A web map has been saved to `output/index.html`. Please check it out ;-)"
+)
